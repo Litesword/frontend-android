f18f0b77a92a93ee7746c4eb7694cacff9fbb8b0
reddyvaritharun <reddyvaritharun@gmail.com>
2024-05-04 18:43:34 +0530

    1st commit

--- App.js
+++ App.js
@@ -10,24 +10,41 @@ import { launchImageLibrary } from "react-native-image-picker";
 import { useState } from "react";
 import  axios from "axios";
 let paper = "Paper2";
-  let result2 =  axios.get(`http://192.168.29.5:8080/add_department/${paper}`).then(response => {
-    result2 = response.data; // Log the response data (array of addDepartment objects)
+  let result2 =  axios.get(`http:http://192.168.178.21:8080/add_department/${paper}`).then(response => {
+    result2 = response.data;
+    console.log(response) // Log the response data (array of addDepartment objects)
   })
+  .catch(error => {
+    // Handle error
+    console.error('Error fetching data:', error);
+  });
   
  paper = "Paper1";
-  let result1 =  axios.get(`http://192.168.29.5:8080/add_department/${paper}`).then(response => {
-    result1 = response.data; // Log the response data (array of addDepartment objects)
+  let result1 =  axios.get(`http://192.168.178.21:8080/add_department/${paper}`).then(response => {
+    result1 = response.data;
+   // Log the response data (array of addDepartment objects)
   })
-
+  .catch(error => {
+    // Handle error
+    console.error('Error fetching data:', error);
+  });
  paper = "Paper3";
-  let result3 =  axios.get(`http://192.168.29.5:8080/add_department/${paper}`).then(response => {
-    result2 = response.data; // Log the response data (array of addDepartment objects)
-  })
-  paper = "Paper4";
-  let result4 =  axios.get(`http://192.168.29.5:8080/add_department/${paper}`).then(response => {
+
+  let result3 =  axios.get(`http://192.168.178.21:8080/add_department/${paper}`).then(response => {
     result3 = response.data; // Log the response data (array of addDepartment objects)
   })
-
+  .catch(error => {
+    // Handle error
+    console.error('Error fetching data:', error);
+  });
+  paper = "Paper4";
+  let result4 =  axios.get(`http://192.168.178.21:8080/add_department/${paper}`).then(response => {
+    result4 = response.data; // Log the response data (array of addDepartment objects)
+  })
+  .catch(error => {
+    // Handle error
+    console.error('Error fetching data:', error);
+  });
 
 function HomeScreen({ navigation }) {
   const insets =useSafeAreaInsets();
@@ -170,10 +187,42 @@ function Notscreen({ navigation }){
 };
 function Department1({ navigation }) {
   const departmentsArray = [];
+  const imageArray=[];
   for(let i = 0;i<result1.length;i++){
     departmentsArray.push(result1[i].departmentName)
+    imageArray.push(result1[i].imagepic)
   }
-  
+  console.log(result1);
+  return (
+    <View>
+      <Text style={styles.de1}>Departments</Text>
+      <View style={styles.deonfoContainer}>
+  {departmentsArray.map((department, index) => (
+    <View key={index} style={styles.deonfo1}>
+      <Text style={styles.depar}>{department}</Text>
+    </View>
+  ))}
+  {imageArray.map((imagepic,index) =>(
+  <View key={index}  >
+      <Image source={{uri:imagepic}} style={{width:'50%',height:'10px'}} ></Image>
+  </View>
+  ))}
+</View>
+    </View>
+  );
+}
+ function Department2({navigation}) {
+  const departmentsArray = [];
+  const imageArray =[];
+  for(let i = 0;i<result2.length;i++){
+    departmentsArray.push(result2[i].departmentName)
+  }
+  for(let i =0;i<result2.length;i++){
+    console.log(result2[i]);
+    imageArray.push(result2[i].imagepic)
+  }
+  console.log(result2)
+
   
   return (
     <View>
@@ -184,26 +233,10 @@ function Department1({ navigation }) {
       <Text style={styles.depar}>{department}</Text>
     </View>
   ))}
-</View>
-    </View>
-  );
-}
-
- function Department2({ navigation }) {
-  const departmentsArray = [];
-  for(let i = 0;i<result2.length;i++){
-    departmentsArray.push(result2[i].departmentName)
-  }
-  console.log(result2)
-  
-  return (
-    <View>
-      <Text style={styles.de1}>Departments</Text>
-      <View style={styles.deonfoContainer}>
-  {departmentsArray.map((department, index) => (
-    <View key={index} style={styles.deonfo1}>
-      <Text style={styles.depar}>{department}</Text>
-    </View>
+  {imageArray.map((imagepic,index) =>(
+  <View key={index}  >
+      <Image source={{uri:imagepic}} style={{width:'50%',height:'10px'}} ></Image>
+  </View>
   ))}
 </View>
     </View>
@@ -212,10 +245,42 @@ function Department1({ navigation }) {
 
 function Department3({ navigation }) {
   const departmentsArray = [];
+  const imageArray =[];
   for(let i = 0;i<result3.length;i++){
     departmentsArray.push(result3[i].departmentName)
   }
-  
+  for(let i =0;i<result3.length;i++){
+    imageArray.push(result3[i].imagepic)
+  }
+  console.log(result3);
+  return (
+    <View>
+      <Text style={styles.de1}>Departments</Text>
+      <View style={styles.deonfoContainer}>
+  {departmentsArray.map((department, index) => (
+    <View key={index} style={styles.deonfo1}>
+      <Text style={styles.depar}>{department}</Text>
+    </View>
+  ))}
+  {imageArray.map((imagepic,index) =>(
+  <View key={index}  >
+      <Image source={{uri:imagepic}} style={{width:'100%',height:'100px'}} ></Image>
+  </View>
+  ))}
+</View>
+    </View>
+  );
+}
+function Department4({ navigation }) {
+  const departmentsArray = [];
+  const imageArray =[];
+  for(let i = 0;i<result4.length;i++){
+    departmentsArray.push(result4[i].departmentName)
+  }
+  for(let i =0;i<result4.length;i++){
+    imageArray.push(result4[i].imagepic)
+  }
+  console.log(result4);
   
   return (
     <View>
@@ -226,25 +291,10 @@ function Department3({ navigation }) {
       <Text style={styles.depar}>{department}</Text>
     </View>
   ))}
-</View>
-    </View>
-  );
-}
-function Department4({ navigation }) {
-  const departmentsArray = [];
-  for(let i = 0;i<result4.length;i++){
-    departmentsArray.push(result4[i].departmentName)
-  }
-  
-  
-  return (
-    <View>
-      <Text style={styles.de1}>Departments</Text>
-      <View style={styles.deonfoContainer}>
-  {departmentsArray.map((department, index) => (
-    <View key={index} style={styles.deonfo1}>
-      <Text style={styles.depar}>{department}</Text>
-    </View>
+  {imageArray.map((imagepic,index) =>(
+  <View key={index}  >
+      <Image source={{uri:imagepic}} style={{width:'50%',height:'10px'}} ></Image>
+  </View>
   ))}
 </View>
     </View>
@@ -339,6 +389,17 @@ height:70,
 width:70,
 marginLeft:29
 },
+pic:{
+    display: 'flex',
+    flexWrap: 'nowrap',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+     borderWidth:'1px',
+     borderRadius:'10px',
+},
+
   inp:{
     marginLeft:50,
     alignContent:"center",
@@ -441,6 +502,7 @@ position:"relative"
   containe: {
     flex: 1,
     padding: 20,
+    flexDirection:'row',
   },
   navigation: {
     flexDirection: "row",
@@ -522,5 +584,5 @@ position:"relative"
   depar:{
     marginLeft:12,
   
-  }
+  },
   });
